df612c7e7b53ce4ab4dd22c6e1b1b322d31bf076
Cristóvão Carlos <cristovo_carlos@hotmail.com>
2024-06-12 09:30:28 -0300

    enviando imagens e readme.md

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello ");
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+};
+const player2 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+};
