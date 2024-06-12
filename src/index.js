525779b4204c271b657a93ac3645fd4ea80da6a2
Cristóvão Carlos <cristovo_carlos@hotmail.com>
2024-06-12 12:15:24 -0300

    configurando os corredores

--- src/index.js
+++ src/index.js
@@ -5,8 +5,51 @@ const player1 = {
     PODER: 3,
 };
 const player2 = {
+    NOME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+};
+
+const Mario = {
     NOME: "Mario",
     VELOCIDADE: 4,
     MANOBRABILIDADE: 3,
     PODER: 3,
 };
+const Bowser = {
+    NOME: "Bowser",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+};
+const Peach = {
+    NOME: "Peach",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+};
+const Luigi = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+};
+const Yoshi = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 2,
+    PODER: 3,
+};
+const Dk = {
+    NOME: "Dk",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+};
+
+
+
+async function main() {
+    
+}
